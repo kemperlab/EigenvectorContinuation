ef2042210d123b2264f9e1dd1eabe3783275454b
jhhoward <jhhoward@ncsu.edu>
2022-08-05 15:05:19 -0400

    File organization

--- continuation_abstraction/examples/test_add_on.py
+++ continuation_abstraction/examples/test_add_on.py
@@ -5,8 +5,12 @@ import numpy as np
 from scipy.linalg import eigh
 from scipy.linalg import null_space
 from matplotlib import pyplot as plt
-from continuation_abstraction import continuation_abstraction as ca
+from src.continuation_abstraction import HilbertSpaceAbstract as hsa
 
+
+# TODO: figure out this import^, make the mimic, and run it
+
+print(hsa.basis_vecs)
 
 
 #%%
